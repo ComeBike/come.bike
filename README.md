178dfcbfe46b9d7e12b62e3367473be353d66330
digitalcake <joshc@digitalcak.es>
2017-12-31 13:37:12 -0800

    init config for coveralls

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 An Open Source Community Site for local bicycling events
 
-[![CircleCI](https://circleci.com/gh/ComeBike/come.bike.svg?style=svg)](https://circleci.com/gh/ComeBike/come.bike)
+[![CircleCI](https://circleci.com/gh/ComeBike/come.bike.svg?style=svg)](https://circleci.com/gh/ComeBike/come.bike) [![Coverage Status](https://coveralls.io/repos/github/ComeBike/come.bike/badge.svg?branch=master)](https://coveralls.io/github/ComeBike/come.bike?branch=master)
--- mix.exs
+++ mix.exs
@@ -6,11 +6,18 @@ defmodule ComeBike.Mixfile do
       app: :come_bike,
       version: "0.0.1",
       elixir: "~> 1.4",
-      elixirc_paths: elixirc_paths(Mix.env),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers,
-      start_permanent: Mix.env == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -26,7 +33,7 @@ defmodule ComeBike.Mixfile do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
   #
@@ -42,7 +49,8 @@ defmodule ComeBike.Mixfile do
       {:gettext, "~> 0.11"},
       {:phauxth, "~> 1.2"},
       {:bcrypt_elixir, "~> 1.0"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:excoveralls, "~> 0.8.0", only: :test}
     ]
   end
 
@@ -56,7 +64,7 @@ defmodule ComeBike.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
