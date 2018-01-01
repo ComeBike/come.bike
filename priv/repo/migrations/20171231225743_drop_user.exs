e7f9626bceccab21b6837c29becf44f498c28edf
digitalcake <joshc@digitalcak.es>
2018-01-01 11:31:53 -0800

    consolidate migrations

--- priv/repo/migrations/20171231225743_drop_user.exs
+++
@@ -1,7 +0,0 @@
-defmodule ComeBike.Repo.Migrations.DropUser do
-  use Ecto.Migration
-
-  def change do
-    drop(table(:users))
-  end
-end
--- priv/repo/migrations/20180101174602_create_users.exs
+++ priv/repo/migrations/20180101174602_create_users.exs
@@ -3,13 +3,14 @@ defmodule ComeBike.Repo.Migrations.CreateUsers do
 
   def change do
     create table(:users) do
-      add :email, :string
-      add :password_hash, :string
-      add :sessions, {:map, :integer}, default: "{}"
+      add(:email, :string)
+      add(:password_hash, :string)
+      add(:sessions, {:map, :integer}, default: "{}")
+      add(:facebook_uid, :string)
 
       timestamps()
     end
 
-    create unique_index :users, [:email]
+    create(unique_index(:users, [:email]))
   end
 end
