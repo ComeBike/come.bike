94225ad1633976a235192eed01dcbf31e60400ae
digitalcake <joshc@digitalcak.es>
2018-01-02 23:52:33 -0800

    add some basic style to the home page

--- lib/come_bike_web/views/page_view.ex
+++ lib/come_bike_web/views/page_view.ex
@@ -1,3 +1,14 @@
 defmodule ComeBikeWeb.PageView do
   use ComeBikeWeb, :view
+
+  def render("header.index.html", assigns) do
+    render(
+      ComeBikeWeb.PageView,
+      "header.html",
+      conn: assigns.conn,
+      current_user: assigns.current_user
+    )
+
+    # {:safe, "<script src=#{Digitalcakes.Router.Helpers.static_path(assigns[:conn], "/js/admin.js")} ></script>"}
+  end
 end
