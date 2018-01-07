8a9f2d2b8367eefa1999148cf19c5848ae71e393
digitalcake <joshc@digitalcak.es>
2018-01-07 00:47:52 -0800

    add push notification

--- lib/come_bike_web/controllers/page_controller.ex
+++ lib/come_bike_web/controllers/page_controller.ex
@@ -1,7 +1,9 @@
 defmodule ComeBikeWeb.PageController do
   use ComeBikeWeb, :controller
+  alias ComeBike.Events
 
   def index(conn, _params) do
-    render conn, "index.html"
+    rides = Events.list_rides()
+    render(conn, "index.html", rides: rides)
   end
 end
